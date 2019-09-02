cc29bbf7d01a33a9683893b9333198ac4ff054d2
Ali Karimi <calirkarimi7@gmail.com>
2019-09-02 10:16:05 +0430

    nothing2

--- src/app/customer/sign-up-customer/sign-up-customer.component.ts
+++ src/app/customer/sign-up-customer/sign-up-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-customer',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpCustomerComponent implements OnInit {
 
+  signUpForm : FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'fname': new FormControl(null),
+      'lname': new FormControl(null),
+      'username': new FormControl(null , Validators.required),
+      'password': new FormControl(null , Validators.required)
+    })
   }
+  
+  onsubmit(){
+    console.log(44555);
+  }
+
 
 }
